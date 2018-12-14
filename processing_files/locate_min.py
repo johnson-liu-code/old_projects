d44f8914b8dc368219b0c96c947f7720a961c7fb
jliu25 <j_liu21@u.pacific.edu>
2018-12-14 00:00:35 -0800

    Some changes

--- processing_files/locate_min.py
+++ processing_files/locate_min.py
@@ -19,7 +19,7 @@ thresh = 1.e-4
 #variables = ['Central Pressure', 'Rc', 'Core Mass Frac Ratio', 'Mass']
 variables = ['Central Pressure', 'Rc', 'Radius', 'Core Mass Frac Ratio']
 #step_size = 5e3
-step_size = 1e4
+#step_size = 1e4 # Sometimes the step size is larger than the difference between Rc1 and Rc2.
 
 algorithm = args.integration_method
 eos = args.eos
@@ -31,12 +31,12 @@ num_layers = '2'
 core_state = 'liquid'
 
 if num_layers == '3':
-    core_boundary = ''
+    #core_boundary = ''
     rho_list = '7.85x7.05x3.98'
     K_list = '255x201x206'
 
 elif num_layers == '2':
-    core_boundary = ''
+    #core_boundary = ''
     # solid iron
     if core_state == 'solid':
         rho_list = '7.85x3.98'
@@ -57,6 +57,7 @@ earth_radius = '6.3781e8'
 earth_mass = '5.972e27'
 
 def find_min(data_list, inde_index, depe_index, desired_y, expected_core_mass_frac, radius_p, mass_p):
+    step_size = 1e4
     print('\nFinding best {} for {} ...\n'.format( variables[inde_index], variables[depe_index] ) )
 
     #print data_list[2]
@@ -71,19 +72,22 @@ def find_min(data_list, inde_index, depe_index, desired_y, expected_core_mass_fr
 #    elif inde_index == 1:
 #        other_inde_index = 0
 
-    for i in data_list[inde_index]:
+    for c, i in enumerate( data_list[inde_index] ):
+        #print 'c', c
+        #print 'i', i
         if inde_index == 0:
-            radii_list = str( data_list[other_inde_index][-1] )
+            radii_list = str( data_list[other_inde_index][1] )
+            #print 'radii_list', radii_list
             #radii_list = core_boundary + str( data_list[other_inde_index][-1] )
             #radii_list = core_boundary + '{0:.10f}'.format( data_list[other_inde_index][-1] )
             pressure = str(i)
             #pressure = '{0:.10f}'.format(i)
 
         elif inde_index == 1:
-            radii_list = core_boundary + str(i)
+            radii_list = str(i)
             #radii_list = str(i)
             #radii_list = core_boundary + '{0:.10f}'.format(i)
-            pressure = str( data_list[other_inde_index][-1] )
+            pressure = str( data_list[other_inde_index][1] )
             #pressure = '{0:.10f}'.format( data_list[other_inde_index][-1] )
 
         final_radius, final_mass, core_mass_frac_ratio = run_integrator( args.file_name, num_layers, radii_list, rho_list, K_list, pressure, step_size, algorithm, eos, radius_p, mass_p, expected_core_mass_frac)
@@ -99,13 +103,24 @@ def find_min(data_list, inde_index, depe_index, desired_y, expected_core_mass_fr
 #    print('depe_variable: ', depe_index, ', inde_variable: ', inde_index)
 
     #print data_list
-    while abs( desired_y - data_list[depe_index][-1] ) > thresh:
+    while abs( desired_y - data_list[depe_index][1] ) > thresh:
+        '''
         if data_list[depe_index][0] - data_list[depe_index][1] == 0:
+            print data_list[depe_index][0], data_list[depe_index][1]
+            print 'hello'
             #print 'break', data_list
             #break
 
+            if data_list[inde_index][0] > data_list[inde_index][1]:
+                larger_variable_index = 0
+                smaller_variable_index = 1
+
+            else:
+                larger_variable_index = 1
+                smaller_variable_index = 0
+
             if data_list[depe_index][0] < 1:
-                data_list[inde_index][1] = 1.1 * data_list[inde_index][1]
+                data_list[inde_index][larger_variable_index] = 1.1 * data_list[inde_index][larger_variable_index]
 
                 if inde_index == 0:
                     pressure = str( data_list[inde_index][1] )
@@ -115,7 +130,7 @@ def find_min(data_list, inde_index, depe_index, desired_y, expected_core_mass_fr
                     radii_list = str( data_list[inde_index][1] )
 
             elif data_list[depe_index][0] > 1:
-                data_list[inde_index][0] = .9 * data_list[inde_index][0]
+                data_list[inde_index][smaller_variable_index] = .9 * data_list[inde_index][smaller_variable_index]
 
                 if inde_index == 0:
                     pressure = str( data_list[inde_index][0] )
@@ -127,44 +142,48 @@ def find_min(data_list, inde_index, depe_index, desired_y, expected_core_mass_fr
             final_radius, final_mass, core_mass_frac_ratio = run_integrator( args.file_name, num_layers, radii_list, rho_list, K_list, pressure, step_size, algorithm, eos, radius_p, mass_p, expected_core_mass_frac)
 
             if data_list[depe_index][0] < 1:
-                data_list[2][1] = final_radius
-                data_list[3][1] = core_mass_frac_ratio
+                data_list[2][larger_variable_index] = final_radius
+                data_list[3][larger_variable_index] = core_mass_frac_ratio
 
             elif data_list[depe_index][0] > 1:
-                data_list[2][0] = final_radius
-                data_list[3][0] = core_mass_frac_ratio
-
+                data_list[2][smaller_variable_index] = final_radius
+                data_list[3][smaller_variable_index] = core_mass_frac_ratio
+        '''
         print 'data: ', data_list
 
-        
-
-        '''
         next_inde_guess = lin_inter( data_list[inde_index], data_list[depe_index], desired_y )
         #print type(next_inde_guess)
         print 'next_inde_guess: {0:.10f}'.format(next_inde_guess)
         #print next_inde_guess
 
         if inde_index == 0:
-            radii_list = core_boundary + str( data_list[other_inde_index][-1] )
+            radii_list = str( data_list[other_inde_index][1] )
             pressure = str(next_inde_guess)
 
             final_radius, final_mass, core_mass_frac_ratio = run_integrator( args.file_name, num_layers, radii_list, rho_list, K_list, pressure, step_size, algorithm, eos, radius_p, mass_p, expected_core_mass_frac )
-            print('pressure: {0}, Rc: {1:.0f}, radius: {2}, mass: {3}, CMFR: {4}'.format(next_inde_guess, data_list[other_inde_index][-1], final_radius, final_mass, core_mass_frac_ratio))
+            print('pressure: {0}, Rc: {1:.0f}, radius: {2}, mass: {3}, CMFR: {4}'.format(next_inde_guess, data_list[other_inde_index][1], final_radius, final_mass, core_mass_frac_ratio))
             #print('pressure: {} Rc: {} core_mass_frac_ratio: {} mass: {}'.format(next_inde_guess, data_list[other_inde_index][-1], core_mass_frac_ratio, final_mass))
             #print core_mass_frac_ratio
         elif inde_index == 1:
-            radii_list = core_boundary + str(next_inde_guess)
-            pressure = str( data_list[other_inde_index][-1] )
+            radii_list = str(next_inde_guess)
+            pressure = str( data_list[other_inde_index][1] )
 
             final_radius, final_mass, core_mass_frac_ratio = run_integrator( args.file_name, num_layers, radii_list, rho_list, K_list, pressure, step_size, algorithm, eos, radius_p, mass_p, expected_core_mass_frac )
-            print('pressure: {0}, Rc: {1:.0f}, radius: {2}, mass: {3}, CMFR: {4}'.format(data_list[other_inde_index][-1], next_inde_guess, final_radius, final_mass, core_mass_frac_ratio))
+            print('pressure: {0}, Rc: {1:.0f}, radius: {2}, mass: {3}, CMFR: {4}'.format(data_list[other_inde_index][1], next_inde_guess, final_radius, final_mass, core_mass_frac_ratio))
             #print('pressure: {} Rc: {} core_mass_frac_ratio: {} mass: {}'.format(data_list[other_inde_index][-1], next_inde_guess, core_mass_frac_ratio, final_mass))
 
         if abs(desired_y - data_list[depe_index][0]) < abs(desired_y - data_list[depe_index][1]):
-            
+            data_list[inde_index] = [ data_list[inde_index][0], next_inde_guess ]
+            data_list[2] = [ data_list[2][0], final_radius ]
+            data_list[3] = [ data_list[3][0], core_mass_frac_ratio ]
 
+        else:
+            data_list[inde_index] = [ data_list[inde_index][1], next_inde_guess ]
+            data_list[2] = [ data_list[2][1], final_radius ]
+            data_list[3] = [ data_list[3][1], core_mass_frac_ratio ]
+        '''
         if abs(desired_y - data_list[depe_index][0]) < abs(desired_y - data_list[depe_index][1]):
-            #data_list[inde_index] = [ data_list[inde_index][0] ]
+            data_list[inde_index] = [ data_list[inde_index][0] ]
             data_list[2] = [ data_list[2][0] ]    # radius
             data_list[3] = [ data_list[3][0] ]    # mass
         else:
@@ -182,6 +201,8 @@ def find_min(data_list, inde_index, depe_index, desired_y, expected_core_mass_fr
         #data_list[3].append(final_mass)
         data_list[3].append(core_mass_frac_ratio)
         '''
+        if np.abs(data[1][0] - data[1][1]) < step_size:
+            step_size = 1e3
 
         #print(abs( 1 - data_list[depe_index][-1] ), thresh)
     #print 'data_list_type', type(data_list)
